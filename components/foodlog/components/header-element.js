18347d238f2464bc3aabe8cf7dcbfa1021173d92
Esma <es_my@hotmail.com>
2022-05-27 13:29:47 +0200

    Updates

--- components/foodlog/components/header-element.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-const Header = styled.h1`
-  padding-bottom: 20px;
-  color: black;
-`;
-export default function HeaderElement(props) {
-  return <Header> {props.header} </Header>;
-}
--- components/ui/EDiaryCard.js
+++
@@ -1,7 +0,0 @@
-import classes from './EDiaryCard.module.css';
-
-function EDiaryCard(props) {
-  return <div className={classes.card}>{props.children}</div>;
-}
-
-export default EDiaryCard;
